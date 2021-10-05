8fa91b55dfb7b6af29cdd5bf56d9b5661f4b88b8
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-06 00:28:53 +0530

    add: added a createCollectible function for minting a new nft

--- contracts/SimpleCollectible.sol
+++ contracts/SimpleCollectible.sol
@@ -4,5 +4,14 @@ pragma solidity 0.6.6;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract SimpleCollectible is ERC721 {
+    uint256 public tokenCounter;
+
     constructor() public ERC721("Dogie", "DOG") {}
+
+    function createCollectible() public view returns (uint256) {
+        uint256 newTokenId = tokenCounter;
+        _safeMint(msg.sender, newTokenId); // minting a new nft with the owner address and the token-id for it
+        tokenCounter += 1;
+        return newTokenId;
+    }
 }
