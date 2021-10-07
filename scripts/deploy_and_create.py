a20300b63ddf908ae457dbcee690542f1fe7fb89
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-08 00:25:14 +0530

    update: update the functio data

--- scripts/deploy_and_create.py
+++ scripts/deploy_and_create.py
@@ -5,7 +5,7 @@ sample_token_uri = "https://ipfs.io/ipfs/Qmd9MCGtdVz2miNumBHDbvj8bigSgTwnr4SbyH6
 OPENSEA_URL = 'https://testnets.opensea.io/assets/{}/{}'
 
 
-def main():
+def deploy_and_create():
     account = get_account()
     simple_collectible = SimpleCollectible.deploy({"from": account})
     transaction = simple_collectible.createCollectible(
@@ -14,3 +14,7 @@ def main():
     print(
         f"Awesome, now you can view your nft at {OPENSEA_URL.format(simple_collectible.address, simple_collectible.tokenCounter() - 1)}")
     return simple_collectible
+
+
+def main():
+    deploy_and_create()
