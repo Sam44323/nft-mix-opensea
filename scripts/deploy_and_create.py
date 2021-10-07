8ed308b89adb1f4a3e04787dd32b34fc8835c80e
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-07 17:39:35 +0530

    add: added required script data for deploying, minting and reviewing a new nft

--- scripts/deploy_and_create.py
+++ scripts/deploy_and_create.py
@@ -1,5 +1,15 @@
+from brownie import SimpleCollectible
 from scripts.helpful_scripts import get_account
+
+sample_token_uri = "https://ipfs.io/ipfs/Qmd9MCGtdVz2miNumBHDbvj8bigSgTwnr4SbyH6DNnpWdt?filename=0-PUG.json"
+OPENSEA_URL = 'https://testnets.opensea.io/assets/{}/{}'
 
 
 def main():
     account = get_account()
+    simple_collectible = SimpleCollectible.deploy({"from": account})
+    transaction = simple_collectible.createCollectible(
+        sample_token_uri, {"from": account})
+    transaction.wait(1)
+    print(
+        f"Awesome, now you can view your nft at {OPENSEA_URL.format(simple_collectible.address, simple_collectible.tokenCounter() - 1)}")
