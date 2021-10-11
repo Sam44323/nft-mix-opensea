f72d2b08337ea7333946d3e030b86b38f83acf04
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-11 23:01:43 +0530

    update: updated the token uri data

--- scripts/advanced_collectible/deploy_and_create.py
+++ scripts/advanced_collectible/deploy_and_create.py
@@ -1,9 +1,6 @@
 from brownie import AdvancedCollectible
 from scripts.utils.helpful_scripts import get_account
 
-sample_token_uri = "https://ipfs.io/ipfs/Qmd9MCGtdVz2miNumBHDbvj8bigSgTwnr4SbyH6DNnpWdt?filename=0-PUG.json"
-OPENSEA_URL = 'https://testnets.opensea.io/assets/{}/{}'
-
 
 def deploy_and_create():
     print('hello')
