40c60ddb744da6f4a0e66ad9bb13122bbf297781
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-11 21:13:42 +0530

    add: added smart contract kit dependecies

--- contracts/AdvancedCollectible.sol
+++ contracts/AdvancedCollectible.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.6.6;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 
 contract AdvancedCollectible is ERC721 {
     constructor() public ERC721("Dogie", "DOG") {}
