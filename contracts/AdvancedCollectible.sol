7e1383dfbe985460e87fe9593ea6b64478db4c1f
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-11 21:21:36 +0530

    add: added basic contract structure for contract in advanced

--- contracts/AdvancedCollectible.sol
+++ contracts/AdvancedCollectible.sol
@@ -4,6 +4,34 @@ pragma solidity 0.6.6;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 
-contract AdvancedCollectible is ERC721 {
-    constructor() public ERC721("Dogie", "DOG") {}
+contract AdvancedCollectible is ERC721, VRFConsumerBase {
+    uint256 public tokenCounter;
+    bytes32 public keyhash;
+    uint256 public fee;
+
+    constructor(
+        address _VRFCoordinator,
+        address _linkToken,
+        bytes32 _keyHash,
+        uint256 _fee
+    )
+        public
+        VRFConsumerBase(_VRFCoordinator, _linkToken)
+        ERC721("Dogie", "DOG")
+    {
+        tokenCounter = 0;
+        keyhash = keyHash;
+        fee = _fee;
+    }
+
+    function createCollectible(string memory tokenURI)
+        public
+        returns (uint256)
+    {
+        uint256 newTokenId = tokenCounter;
+        _safeMint(msg.sender, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+        tokenCounter += 1;
+        return newTokenId;
+    }
 }
