9462de4e6ce454e9aabdb6396906cf4ebe43f087
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-11 22:47:51 +0530

    update: updated the deploy script for advanced

--- scripts/advanced_collectible/deploy_and_create.py
+++ scripts/advanced_collectible/deploy_and_create.py
@@ -1,4 +1,4 @@
-from brownie import SimpleCollectible
+from brownie import AdvancedCollectible
 from scripts.utils.helpful_scripts import get_account
 
 sample_token_uri = "https://ipfs.io/ipfs/Qmd9MCGtdVz2miNumBHDbvj8bigSgTwnr4SbyH6DNnpWdt?filename=0-PUG.json"
