db992975ebce8bccea20a092463b9a642c4cd226
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-12 22:02:25 +0530

    update: updated the deploy script for advanced

--- scripts/advanced_collectible/deploy_and_create.py
+++ scripts/advanced_collectible/deploy_and_create.py
@@ -1,10 +1,15 @@
-from brownie import AdvancedCollectible
-from scripts.utils.helpful_scripts import get_account, OPENSEA_URL
+from brownie import AdvancedCollectible, config, network
+from scripts.utils.helpful_scripts import get_account, OPENSEA_URL, get_contract
 
 
 def deploy_and_create():
     account = get_account()
-    advanced_collectible = AdvancedCollectible.deploy({"from": account})
+    advanced_collectible = AdvancedCollectible.deploy(
+        get_contract('vrf_coordinator'),
+        get_contract('link_token'),
+        config["networks"][network.show_active()]["keyhash"],
+        config["networks"][network.show_active()]["fee"],
+        {"from": account})
 
 
 def main():
