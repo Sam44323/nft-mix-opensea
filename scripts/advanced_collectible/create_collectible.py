8952b997299fb70767e02c11c0bc701f9898dce9
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-15 16:12:47 +0530

    add: added scripts for creating a collectible

--- scripts/advanced_collectible/create_collectible.py
+++ scripts/advanced_collectible/create_collectible.py
@@ -1,4 +1,14 @@
+import web3
+from brownie import AdvancedCollectible
+from scripts.utils.helpful_scripts import fund_with_link, get_account
+from web3 import Web3
 
 
 def main():
-    pass
+    account = get_account()
+    advanced_collectible = AdvancedCollectible[-1]
+    fund_with_link(
+        advanced_collectible.address, amount=Web3.toWei(0.1, 'ether'))
+    transaction = advanced_collectible.createCollectible({"from": account})
+    transaction.wait(1)
+    print("Collectible creation is done!")
