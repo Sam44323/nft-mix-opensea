e62c985108b4d762f7668dc7a57ddc567448dadf
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-15 19:35:14 +0530

    add: added metadata file checker method using pathlib

--- scripts/advanced_collectible/create_metadata.py
+++ scripts/advanced_collectible/create_metadata.py
@@ -1,7 +1,7 @@
 from brownie import AdvancedCollectible, network
 from scripts.utils.helpful_scripts import get_breed
 from metadata.sample_metadata import metadata_template
-import pathlib
+from pathlib import Path
 
 
 def main():
@@ -12,3 +12,5 @@ def main():
         breed = get_breed(advanced_collectible.tokenIdToBreed(tokenId))
         metadata_filename = f'../../metadata/{network.show_active()}/{tokenId}-{breed}'
         print(metadata_filename)
+        if Path(metadata_filename).exists():
+            print(f"{metadata_filename} already exists! Delete to overwrite it.")
