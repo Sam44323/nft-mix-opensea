7dde3c1233f8d6bc6aef4209ccc85e9e6dea6f31
Suranjan <suranjanbiswas2002@gmail.com>
2021-10-15 18:50:44 +0530

    add: added the file for creating a metadata for tokens

--- scripts/advanced_collectible/deploy_and_create.py
+++ scripts/advanced_collectible/deploy_and_create.py
@@ -1,18 +1,23 @@
-from brownie import AdvancedCollectible, config, network
+from brownie import AdvancedCollectible, network, config
 from scripts.utils.helpful_scripts import get_account, OPENSEA_URL, get_contract, fund_with_link
 
 
 def deploy_and_create():
     account = get_account()
+    # We want to be able to use the deployed contracts if we are on a testnet
+    # Otherwise, we want to deploy some mocks and use those
+    # Rinkeby
     advanced_collectible = AdvancedCollectible.deploy(
-        get_contract('vrf_coordinator'),
-        get_contract('link_token'),
+        get_contract("vrf_coordinator"),
+        get_contract("link_token"),
         config["networks"][network.show_active()]["keyhash"],
         config["networks"][network.show_active()]["fee"],
-        {"from": account}, publish_source=config["networks"][network.show_active()].get("verify", False))
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get(
+            "verify", False)
+    )
     fund_with_link(advanced_collectible.address)
     creating_tx = advanced_collectible.createCollectible({"from": account})
-
     creating_tx.wait(1)
     print("New token has been created!")
     return advanced_collectible, creating_tx
